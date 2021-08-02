7806454123a2452f9af3ce6a6f6df12a8ec2525f
Adrien RICHARD <richardadrien0@gmail.com>
2021-08-02 23:43:16 +0200

    addded pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ExchangeDetailsComponent } from './pages/exchange-details/exchange-details.component';
+import { ExchangeListComponent } from './pages/exchange-list/exchange-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'exchanges', component: ExchangeListComponent },
+  { path: 'exchanges/:id', component: ExchangeDetailsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Pages
+import { ProfileComponent } from './pages/profile/profile.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExchangeListComponent } from './pages/exchange-list/exchange-list.component';
+import { ExchangeDetailsComponent } from './pages/exchange-details/exchange-details.component';
+import { AboutComponent } from './pages/about/about.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    ExchangeListComponent,
+    ExchangeDetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
